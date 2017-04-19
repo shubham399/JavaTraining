bcac1c05fd4256ef17491089c0cd30cfdbe2fad1
shubham399 <shubham.399@gmail.com>
2017-04-19 19:54:07 +0530

    New Uploads

---
+++ Assignment/Assignment3.java
@@ -0,0 +1,51 @@
+/*
+n t
+2 3
+arr
+4 5
+start dest
+5 7   //YES
+*/
+import java.util.Scanner;
+class Assignment3
+{
+  public static void main(String args[])
+  {
+    int p,n,d,t;
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    t=s.nextInt();
+    int z[] = new int[n];
+    for(int i=0;i<n;i++)
+    z[i]=s.nextInt();
+    p=s.nextInt();
+    d=s.nextInt();
+    for(int i=0;i<n;i++)
+    {
+      for(int j=1;j<(n-i);j++)
+      {
+        if(z[j-1]>z[j])
+        {
+          int temp=z[j];
+          z[j]=z[j-1];
+          z[j-1]=temp;
+        }
+      }
+    }
+    int i=0;
+    while(d>p || i<n || t>0)
+    {
+		t--;
+      int diff=p-z[i];
+      if(diff<0)
+      break;
+      else
+      p=p+diff;
+      i++;
+    }
+    if(d<=p)
+    System.out.println("Yes");
+    else
+    System.out.println("No");
+  }
+}
